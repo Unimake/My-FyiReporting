b7ca94d4420929cd35c0eaa9238ad0ffa06a5e1e
marcelo-unimake <marcelo@unimake.com.br>
2014-09-23 15:11:26 -0300

    Ajustes no modo release
    Foi corrigido o arquivo de .bat para facilitar o debug pelo OpenPOS

--- Send2OpenPOS.bat
+++ Send2OpenPOS.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 CLS
+DIR
 
 @ECHO =======================================
 @ECHO   Compilando projeto em modo release
@@ -10,22 +11,22 @@ CALL "%ProgramFiles% (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" 
 @ECHO =======================================
 @ECHO    Copiando arquivos para o OpenPOS
 @ECHO =======================================
-COPY RdlDesign\bin\x86\Release\DataProviders.dll			 ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence	/Y
-COPY RdlDesign\bin\x86\Release\ICSharpCode.SharpZipLib.dll  ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\itextsharp.dll               ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlCri.dll                   ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlCri.pdb                   ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlDesigner.exe              ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlDesigner.exe.config       ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlEngine.dll                ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlEngineConfig.Linux.xml    ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlEngineConfig.xml          ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\RdlViewer.dll                ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
-COPY RdlDesign\bin\x86\Release\zxing.dll                    ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence   /Y
+COPY DataProviders.dll			  ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY ICSharpCode.SharpZipLib.dll  ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY itextsharp.dll               ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlCri.dll                   ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlCri.pdb                   ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlDesigner.exe              ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlDesigner.exe.config       ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlEngine.dll                ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlEngineConfig.Linux.xml    ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlEngineConfig.xml          ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY RdlViewer.dll                ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
+COPY zxing.dll                    ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence /Y
 
-COPY ..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence\*.* ..\..\OpenPOS\OpenPOS\trunk\fontes\OpenPOS\Reports\OpenPOS.Reports\Embedded\ /Y
+COPY ..\..\..\..\..\OpenPOS\OpenPOS\trunk\references\MajorSilence\*.* ..\..\..\..\..\OpenPOS\OpenPOS\trunk\fontes\OpenPOS\Reports\OpenPOS.Reports\Embedded\ /Y
 
 @ECHO =======================================
 @ECHO    Processo concluido
 @ECHO =======================================
-pause
+PAUSE
